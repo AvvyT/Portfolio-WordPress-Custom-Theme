f1275c322de6800bac539d4e493db11847d6d6e9
Radoslava Tsankova Todorova <radoslavatsankovatodorova@MacBook-de-Radoslava.local>
2020-02-28 14:46:17 +0100

    downArrow improved css

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'avvy_portfolio' );
+define( 'DB_NAME', 'wordPress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'wpuser' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'wppass' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
